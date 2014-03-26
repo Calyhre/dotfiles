b11ae4e18e04e19a18a8b7e754905850d76ceefa
Calyhre <calyhre@gmail.com>
2014-03-26 15:13:47 +0100

    Ask me before, cause sometimes I'm not sure

--- home/.scripts/deploy.rb
+++ home/.scripts/deploy.rb
@@ -3,9 +3,17 @@
 trap("SIGINT") { puts "\n"; exit! }
 
 require 'logger'
-require 'pry'
 require 'git'
 require 'highline/import'
+
+def are_you_sure?( question )
+  print "#{question} [y/n]: "
+  if gets.strip.downcase =~ /^(y|yes)$/
+    return true
+  else
+    return false
+  end
+end
 
 system "cd `git rev-parse --show-toplevel`"
 puts '# For debugging informations about this deploy, please see'
@@ -21,13 +29,15 @@ if heroku_remotes.count == 1
 else
   puts 'Remotes available :'
   choose do |menu|
-    menu.prompt = "\nPlease choose the remote to deploy to ? "
+    menu.prompt = "\nPlease choose the remote to deploy to : "
     menu.choices *heroku_remotes.map(&:name) do |remote|
       @remote = remote
     end
   end
-  puts "\n"
 end
 
-puts "Deploying branch #{git.current_branch} on #{@remote}\n"
-system "git push #{@remote} #{git.current_branch}:master"
+if are_you_sure? "Deploying branch #{git.current_branch} on #{@remote} ?"
+  system "git push #{@remote} #{git.current_branch}:master"
+else
+  abort 'Nothing was made. Keep cool :)'
+end
